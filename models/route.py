54680d415591924d5d6db58ac17928edd6ab8a6c
Lore1245 <sarmientoloreley@gmail.com>
2025-11-19 13:20:05 -0500

    Fix ForeignKey reference for user_id in route model

--- models/route.py
+++ models/route.py
@@ -23,7 +23,7 @@ class RouteCalculated(Base):
 
     id = Column("route_id", Integer, primary_key=True, index=True)
 
-    user_id = Column("user_id", Integer, ForeignKey("Usuarios.id"), nullable=False)
+    user_id = Column("user_id", Integer, ForeignKey("Usuarios.user_id"), nullable=False)
     origin_id = Column("origin_id", Integer, ForeignKey("Aeropuertos.airport_id"), nullable=False)
     destiny_id = Column("destiny_id", Integer, ForeignKey("Aeropuertos.airport_id"), nullable=False)
 

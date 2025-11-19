a20ec1ef6d7433cb8bf310eca1f3a38f4b2c87b6
Lore1245 <sarmientoloreley@gmail.com>
2025-11-19 13:41:58 -0500

    Update user.py

--- models/user.py
+++ models/user.py
@@ -7,7 +7,7 @@ class User(Base):
     __tablename__ = "Usuarios"
 
     id = Column("user_id", Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, index=True, nullable=False)
+    username = Column("username",String(50), unique=True, index=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
     registered_at = Column(DateTime(timezone=True), server_default=func.now())
 

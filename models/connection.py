64db190290d3f5e94396c2eb21efe91f0bfcaa50
Lore1245 <sarmientoloreley@gmail.com>
2025-11-19 11:29:21 -0500

    Refactor Connection model column definitions
    Updated column names and foreign key references in the Connection model.

--- models/connection.py
+++ models/connection.py
@@ -6,11 +6,23 @@ from db.base import Base
 class Connection(Base):
     __tablename__ = "Conexiones"
 
-    id = Column(Integer, primary_key=True, index=True)  # connection_id
-    airport_a_id = Column(Integer, ForeignKey("airports.id"), nullable=False)
-    airport_b_id = Column(Integer, ForeignKey("airports.id"), nullable=False)
-    distance_km = Column(Float, nullable=False)
-    congestion_factor = Column(Float, nullable=False, default=1.0)
+    id = Column("conection_id", Integer, primary_key=True, index=True)
+
+    airport_a_id = Column(
+        "airport_a",
+        Integer,
+        ForeignKey("Aeropuertos.airport_id"),
+        nullable=False,
+    )
+    airport_b_id = Column(
+        "airport_b",
+        Integer,
+        ForeignKey("Aeropuertos.airport_id"),
+        nullable=False,
+    )
+
+    distance_km = Column("distance", Float, nullable=False)
+    #congestion_factor = Column(Float, nullable=False, default=1.0)  # si no existe, quítala
     cost = Column(Numeric(10, 2), nullable=False)
 
     airport_a = relationship("Airport", foreign_keys=[airport_a_id], back_populates="connections_from")

bfb0d5b3de574d8cda1ecf996bdb69253d4ee988
Lore1245 <sarmientoloreley@gmail.com>
2025-11-19 13:19:34 -0500

    Update user.py

--- models/user.py
+++ models/user.py
@@ -6,7 +6,7 @@ from db.base import Base
 class User(Base):
     __tablename__ = "Usuarios"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column("user_id", Integer, primary_key=True, index=True)
     username = Column(String(50), unique=True, index=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
     registered_at = Column(DateTime(timezone=True), server_default=func.now())

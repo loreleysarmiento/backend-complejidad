2ce46ad950bc590160b54f09dd7c8529059f1f0a
Lore1245 <sarmientoloreley@gmail.com>
2025-11-19 11:29:50 -0500

    Update route.py

--- models/route.py
+++ models/route.py
@@ -1,10 +1,3 @@
-import enum
-
-class RouteCriteriaEnum(str, enum.Enum):
-    DISTANCE = "distance"
-    COST = "cost"
-
-
 from sqlalchemy import (
     Column,
     Integer,
@@ -28,16 +21,18 @@ class RouteCriteriaEnum(str, enum.Enum):
 class RouteCalculated(Base):
     __tablename__ = "RutasCalculadas"
 
-    id = Column(Integer, primary_key=True, index=True)  # route_id
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    origin_id = Column(Integer, ForeignKey("airports.id"), nullable=False)
-    destiny_id = Column(Integer, ForeignKey("airports.id"), nullable=False)
+    id = Column("route_id", Integer, primary_key=True, index=True)
+
+    user_id = Column("user_id", Integer, ForeignKey("Usuarios.user_id"), nullable=False)
+    origin_id = Column("origin_id", Integer, ForeignKey("Aeropuertos.airport_id"), nullable=False)
+    destiny_id = Column("destiny_id", Integer, ForeignKey("Aeropuertos.airport_id"), nullable=False)
+
     total_distance = Column(Float, nullable=False)
     total_cost = Column(Numeric(10, 2), nullable=False)
     query_date = Column(DateTime(timezone=True), server_default=func.now())
-    criteria = Column(String(20), nullable=False)  
-    total_stops = Column(Integer, nullable=False, default=0)
-    algorithm = Column(String(30), nullable=False, default="dijkstra")
+    #criteria = Column(String(20), nullable=False)
+    #total_stops = Column(Integer, nullable=False, default=0)
+    #algorithm = Column(String(30), nullable=False, default="dijkstra")
 
     user = relationship("User", back_populates="routes")
     details = relationship("RouteDetail", back_populates="route", cascade="all, delete-orphan")
@@ -46,9 +41,21 @@ class RouteCalculated(Base):
 class RouteDetail(Base):
     __tablename__ = "DetalleRuta"
 
-    id = Column(Integer, primary_key=True, index=True)  # detail_id
-    route_id = Column(Integer, ForeignKey("routes_calculated.id"), nullable=False)
-    route_order = Column(Integer, nullable=False)  # 0..n
-    airport_id = Column(Integer, ForeignKey("airports.id"), nullable=False)
+    id = Column("detail_id", Integer, primary_key=True, index=True)
+
+    route_id = Column(
+        "route_id",
+        Integer,
+        ForeignKey("RutasCalculadas.route_id"),
+        nullable=False,
+    )
+
+    route_order = Column(Integer, nullable=False)
+    airport_id = Column(
+        "airport_id",
+        Integer,
+        ForeignKey("Aeropuertos.airport_id"),
+        nullable=False,
+    )
 
     route = relationship("RouteCalculated", back_populates="details")
